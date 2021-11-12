e8784cda8f390be38f21888dc66829d1c95e7e6c
Лена <antonio57banderas@gmail.com>
2021-11-13 00:25:45 +0300

    fixed templates

--- query_handling/static/query_handling/javascipt_files/nav_bar.js
+++ query_handling/static/query_handling/javascipt_files/nav_bar.js
@@ -1,47 +1,52 @@
-let nav_search =  document.getElementById('nav-search');
-let nav_links = document.getElementsByClassName('nav-links')[0];
-let hamburger = document.getElementsByClassName('ham-nav')[0];
-console.log(nav_links, hamburger);
-let nav_about =  document.getElementById('nav-about');
-let nav_add_movies =  document.getElementById('nav-add-movies');
-let nav_home =  document.getElementById('nav-home');
-let search_button_html = "<button><i class=\"fa fa-search\" style=\"font-size:0.8rem\"></i></button>";
+let headerGroup = document.getElementsByClassName('header-group')[0];
+let hamburger = document.getElementsByClassName('hamburger')[0];
 
-nav_search.addEventListener('click', function(event) {
-    if((nav_search.children[0].tagName !== "BUTTON") & (event.target.tagName == 'I')){
-        nav_search.innerHTML = search_button_html;
-    } else if (nav_search.children[0].tagName == "BUTTON"){
-        nav_search.innerHTML = create_form_html();
+let search =  document.getElementById('header-search');
+let about =  document.getElementById('header-about');
+let addMovies =  document.getElementById('header-addmovies');
+let home =  document.getElementById('header-home');
+
+const searchIcon = "<i class='fa fa-search' style='font-size: 0.8rem'></i>";
+
+
+
+search.addEventListener('click', function(event) {
+    if((search.children[0].tagName !== "BUTTON") & (event.target.tagName == 'I')){
+        search.innerHTML = "<button>" + searchIcon + "</button>";
+    } else if (search.children[0].tagName == "BUTTON"){
+        search.innerHTML = create_form_html();
     };
 });
 
-nav_about.addEventListener('click', function() {
+
+
+about.addEventListener('click', function() {
     let url = encodeURI('/about');
     window.open(url, '_self');
 }
 );
 
-nav_home.addEventListener('click', function() {
+home.addEventListener('click', function() {
     let url = encodeURI('/');
     window.open(url, '_self');
 }
 );
 
-nav_add_movies.addEventListener('click', function() {
+addMovies.addEventListener('click', function() {
     let url = encodeURI('/addmovies');
     window.open(url, '_self');
     }
 );
 
 hamburger.addEventListener('click', function() {
-    nav_links.classList.toggle('active');
+    headerGroup.classList.toggle('active');
 }
 );
 
 function create_form_html(){
-    let input_html =  "<input type='text' id='query' name='query' class='form-control form-input' style='font-size:0.8rem;height:27px' placeholder='Search Movies...'>";
-    let search_symbol =  "<i class='fa fa-search' style='font-size:0.8rem; top:9px; left:15px'></i>";
-    let form_html = "<form autocomplete='off' action='/search/' method='get' class='form' style='max-width:170px;box-shadow: 0 0 1em 0.1em rgb(100 132 179);'>" + 
+    let input_html =  "<input type='text' id='query' name='query' class='header-form-input' placeholder='Search Movies...'>";
+    let search_symbol =  "<i class='fa fa-search header-search-icon'></i>";
+    let form_html = "<form autocomplete='off' action='/search/' method='get' class='header-form'>" + 
                     search_symbol + input_html + "</form>";
     return form_html;
 }
